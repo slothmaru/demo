e0a8658c503de71b7cccbbb59b998715874a71ad
sloth <slothmaru@gmail.com>
2022-12-09 18:23:08 +0900

    first commit!

---
+++ src/main/java/com/example/demo/dto/TodoDTO.java
@@ -0,0 +1,16 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TodoDTO {
+    private String id;
+    private String title;
+    private boolean done;
+}
